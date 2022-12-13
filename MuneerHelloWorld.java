f50b96a4898320683043a727cf913d1b7e14e85b
modowa <120430092+modowa@users.noreply.github.com>
2022-12-12 19:08:55 -0600

    used integers and if statement to write Hello Tutorial

---
+++ MuneerHelloWorld.java
@@ -0,0 +1,8 @@
+public class MuneerHelloWorld {
+    public static void main(String[] args) {
+        int x = 6;
+        int y = 18;
+        if (x == 6 && y == 18);
+        System.out.println("Hello Tutorial");
+    }
+}
